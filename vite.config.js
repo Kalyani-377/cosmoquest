e9054b8b69f06eddca1439975359dcab1bd63202
kalyani-377 <bnkalyani3707@gmail.com>
2026-03-03 15:52:21 +0530

    Fix 404 routing and optimize build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('@clerk')) return 'vendor-clerk';
+            if (id.includes('@supabase')) return 'vendor-supabase';
+            return 'vendor'; // all other deps
+          }
+        }
+      }
+    }
+  }
 })
